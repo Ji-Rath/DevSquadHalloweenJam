e5b31db58e2868bc45bd56c0db5bd8e8c79091f5
Ji-Rath <garethaguilar@gmail.com>
2021-10-30 12:15:10 -0500

    Add Initial files

---
+++ Source/DevSquadHalloweenJam/DevSquadHalloweenJamGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DevSquadHalloweenJamGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEVSQUADHALLOWEENJAM_API ADevSquadHalloweenJamGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
